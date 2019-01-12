4dfba42a912b8468fce74def4f58f261ef3b221f
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-12 20:17:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,7 +54,7 @@ end
 def runner
   welcome 
   initial_round
-  display_card_total(card_total)
+  display_card_total(initial_round)
   until display_card_total > 21
       hit?(display_card_total)
   end
